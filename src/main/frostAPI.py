bb7b14d2ed3366d6e28d9c16542725a3f15a63fd
kasper280403 <kasper.s.karlsen@gmail.com>
2025-09-23 10:55:43 +0200

    Starting to come togheter, fixed plotting to some deegree

--- src/main/frostAPI.py
+++ src/main/frostAPI.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import requests
 import pandas as pd
 
@@ -36,4 +38,27 @@ def request_response(client_id, endpoint, parameters):
 params_blindern = set_parameters("SN18700", "2024-09-14/2025-09-14", "mean(air_temperature P1D)")
 blindern_df = request_response(client_id, endpoint, params_blindern)
 
-print(blindern_df.head(20))
+params_aas = set_parameters("SN17850", "2024-09-14/2025-09-14", "mean(air_temperature P1D)")
+aas_df = request_response(client_id, endpoint, params_aas)
+
+blindern_df["date"] = pd.to_datetime(blindern_df["date"])
+aas_df["date"] = pd.to_datetime(aas_df["date"])
+
+plt.plot(blindern_df["date"], blindern_df["value"])
+plt.plot(aas_df["date"],aas_df["value"])
+
+plt.gca().xaxis.set_major_locator(mdates.MonthLocator())  # én tick per måned
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))  # format som ÅÅÅÅ-MM
+plt.xticks(rotation=45)
+
+plt.show()
+
+merged_df = pd.merge(blindern_df, aas_df, on="date", suffixes=("_blindern", "_aas"))
+
+# Lag scatterplot
+plt.scatter(merged_df["value_blindern"], merged_df["value_aas"], color="green")
+plt.xlabel("Blindern temperatur (°C)")
+plt.ylabel("Aas temperatur (°C)")
+plt.title("Sammenligning av daglig gjennomsnittstemperatur")
+plt.grid(True)
+plt.show()
