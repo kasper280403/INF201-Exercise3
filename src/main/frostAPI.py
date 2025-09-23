2c8293138701450d0a5e069570a2c5c4e0d619da
kasper280403 <kasper.s.karlsen@gmail.com>
2025-09-23 10:02:54 +0200

    Yeeee it works

--- src/main/frostAPI.py
+++ src/main/frostAPI.py
@@ -1,32 +1,39 @@
 import requests
 import pandas as pd
 
-
 client_id = "d43fc53b-9a43-49c3-a2b2-f27fca2a29d4"
-
 endpoint = "https://frost.met.no/observations/v0.jsonld"
 
-parameters = {
-    "sources": "SN18700",
-    "referencetime": "2024-09-14/2025-09-14",
-    "elements": "mean(air_temperature P1D)"
-}
 
-response = requests.get(endpoint, params=parameters, auth=(client_id, ""))
+def set_parameters(sources, referencetime, elements):
+    parameters = {
+        "sources": sources,
+        "referencetime": referencetime,
+        "elements": elements
+    }
 
-if response.status_code == 200:
-    data = response.json()
-    records = []
-    for item in data.get("data", []):
-        date = item["referenceTime"][:10]
-        for obs in item["observations"]:
-            records.append({
-                "date": date,
-                "value": obs["value"],
-                "unit": obs["unit"]
-            })
+    return parameters
 
-    df = pd.DataFrame(records)
-    print(df.head(20))
-else:
-    print("Error retrieving data:", response.status_code, response.text)
+def request_response(client_id, endpoint, parameters):
+    response = requests.get(endpoint, params=parameters, auth=(client_id, ""))
+    if response.status_code == 200:
+        data = response.json()
+        records = []
+        for item in data.get("data", []):
+            date = item["referenceTime"][:10]
+            for obs in item["observations"]:
+                records.append({
+                    "date": date,
+                    "value": obs["value"],
+                    "unit": obs["unit"]
+                })
+
+        return pd.DataFrame(records)
+
+    else:
+        print("Error retrieving data:", response.status_code, response.text)
+
+params_blindern = set_parameters("SN18700", "2024-09-14/2025-09-14", "mean(air_temperature P1D)")
+blindern_df = request_response(client_id, endpoint, params_blindern)
+
+print(blindern_df.head(20))
